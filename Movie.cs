8e938049e44d8b239eee6b5c07e4f502507c4ae5
Jeremiah Fischer <fischgeek@gmail.com>
2019-09-17 13:51:54 -0500

    quick

---
+++ Movie.cs
@@ -0,0 +1,29 @@
+﻿using TrelloUtility;
+
+namespace TheMovieDBClassLibrary
+{
+    public class Movie : TrelloCard
+    {
+        public string Title
+        {
+            get {
+                return this.name;
+            }
+            set {
+                this.Title = value;
+                this.name = value;
+            }
+        }
+        public string Description
+        {
+            get {
+                return this.desc;
+            }
+            set {
+                this.Description = value;
+                this.desc = value;
+            }
+        }
+        public string Poster { get; set; }
+    }
+}
